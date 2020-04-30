9a059d32c7dd2e9d598525018c4e9dfd6e23859a
Mat191126 <matprz11@gmail.com>
2020-04-30 10:51:59 +0200

    SingleValue.java methods added

--- src/main/java/org/example/Singlevalue.java
+++ src/main/java/org/example/Singlevalue.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class Singlevalue {
+import java.util.List;
+
+public class Singlevalue extends Value{
+    @Override
+    public List<String> getInputPattern() {
+        return null;
+    }
+
+    @Override
+    public boolean getSelectionType() {
+        return false;
+    }
+
+    public Singlevalue(String param, boolean selectionType){
+
+    }
 }
