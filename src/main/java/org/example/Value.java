bfef056ebf4ab938f7d846fdd2f3541d586345ea
Mat191126 <matprz11@gmail.com>
2020-04-30 10:49:54 +0200

    Abstract Value.java methods added

--- src/main/java/org/example/Value.java
+++ src/main/java/org/example/Value.java
@@ -1,4 +1,10 @@
 package org.example;
 
+import java.util.List;
+
 public abstract class Value {
+
+    public abstract List<String> getInputPattern();
+
+    public abstract boolean getSelectionType();
 }
