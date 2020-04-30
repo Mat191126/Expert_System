270421f73cfcf4b87cb2fe5b858e4cce35da3915
Mat191126 <matprz11@gmail.com>
2020-04-30 09:47:12 +0200

    Created ESProwider, RuleParser, FactParser classes

---
+++ src/main/java/org/example/ESProwider.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class ESProwider {
+
+    public ESProwider (FactParser factParser, RuleParser ruleParser){
+
+    }
+
+    public void collectAnswers(){
+
+    }
+
+    public boolean getAnswerByQuestion(String qusetionID){
+
+    }
+
+    public String evaluate(){
+        return "string";
+    }
+
+
+}
