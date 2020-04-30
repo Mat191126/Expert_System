b58bc4b0f073450fbf0eefda101ad7470f68fbbb
Mat191126 <matprz11@gmail.com>
2020-04-30 14:57:47 +0200

    Added Fact class with empty methods

---
+++ src/main/java/org/example/Fact.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Set;
+
+public class Fact {
+
+    public Fact(String ID, String description){
+
+    }
+
+    public Set<String> getIDSet(){
+        return null;
+    }
+
+    public void setFactValueByID(String ID, boolean value){
+
+    }
+
+    public boolean getValueByID(String ID){
+        return true;
+    }
+
+    public String getDescription(){
+        return "description";
+    }
+}
