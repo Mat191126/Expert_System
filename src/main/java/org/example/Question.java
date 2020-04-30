ff5de830d5594b9f9d14f72e86702dc0a6471a15
Mat191126 <matprz11@gmail.com>
2020-04-30 10:08:37 +0200

    Added empty methods in Question.java

--- src/main/java/org/example/Question.java
+++ src/main/java/org/example/Question.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Question {
+    private Answer answer;
+
+    public Question(String ID, String question, Answer answer){
+
+    }
+    public String getID(){
+        return "ID";
+    }
+
+    public String getQuestion(){
+        return "question";
+    }
+
+    public Answer getAnswer(){
+        return this.answer;
+    }
+
+    public boolean getEvaluatedAnswer(String input){
+        return true;
+    }
 }
