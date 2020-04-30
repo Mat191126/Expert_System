5e79af41c6a8006f2e075519fdf266d6a3aa2935
Mat191126 <matprz11@gmail.com>
2020-04-30 15:51:19 +0200

    Iterator refactor

--- src/main/java/org/example/FactRepository.java
+++ src/main/java/org/example/FactRepository.java
@@ -1,11 +1,40 @@
 package org.example;
 
-public class FactRepository {
+public class FactRepository implements Container{
+    public String[] facts = {"A", "B", "C"};
+
     public void addFact(Fact fact){
 
     }
 
-    public Iterator<Fact> getIterator(){
+    /*public Iterator<Fact> getIterator(){
         return null;
+    }*/
+
+    @Override
+    public Iterator getIterator() {
+        return new FactIterator();
+    }
+
+    private class FactIterator implements Iterator{
+        int index;
+
+        @Override
+        public boolean hasNext() {
+
+            if(index < facts.length){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+
+            if(this.hasNext()){
+                return facts[index++];
+            }
+            return null;
+        }
     }
 }
