085c601c3bd795efcaf837021dc151fe4487a818
Mat191126 <matprz11@gmail.com>
2020-06-16 14:03:49 +0200

    RuleParser refactor, add getAnswer method creating Answer object

--- src/main/java/org/example/model/Answer.java
+++ src/main/java/org/example/model/Answer.java
@@ -1,6 +1,11 @@
 package org.example.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Answer {
+    private List<Value> values = new ArrayList<>();
+
     public boolean evaluateAnswerByInput(String input){
         return true;
     }
